1a0047362bd52b6a39a1c73e4f9b73b355a44b9b
TRU <trunangst@gmail.com>
2020-04-08 14:47:01 -0500

    fixed typo in googleapikey

--- my-react-app/src/pages/AddPage/MapSection.js
+++ my-react-app/src/pages/AddPage/MapSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import MapComponent from '../../extra/googleMaps/MapComponent';
 // import key from '../../extra/hiddenFolder/hiddenFile';
-const googleKey = process.env.GoogleKey;
+const googleKey = process.env.googleKey;
 const mapSection = ({ eyesArr }) => {
     return (
         <section className='mapSection'>
