409c72c1cb8748933cbe4f9cad0e34035464a24e
TRU <trunangst@gmail.com>
2020-04-08 14:15:37 -0500

    removed hidden key in map section

--- my-react-app/src/pages/AddPage/MapSection.js
+++ my-react-app/src/pages/AddPage/MapSection.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import MapComponent from '../../extra/googleMaps/MapComponent';
-import key from '../../extra/hiddenFolder/hiddenFile';
-
+// import key from '../../extra/hiddenFolder/hiddenFile';
+const googleKey = key() || process.env.GoogleKey;
 const mapSection = ({ eyesArr }) => {
     return (
         <section className='mapSection'>
             <MapComponent
                 eyesArr={eyesArr}
-                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${key()}`}
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${googleKey}`}
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ height: `100%` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
